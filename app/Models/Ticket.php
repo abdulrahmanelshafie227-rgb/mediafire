dec2c6539352824eb1fffeadead8faa745d872d8
abdulrahmanelshafie227-rgb <abdulrahmanelshafie227@gmail.com>
2026-03-28 16:13:08 +0200

    Create Ticket model file

---
+++ app/Models/Ticket.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model {
+    protected $fillable = ['user_id', 'order_id', 'subject', 'message', 'status', 'priority'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+}
